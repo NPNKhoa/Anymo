37127cc99a80cc7382d5b1b118f877a8d1e290bd
NPNKhoa <khoa.nphucnguyen@gmail.com>
2023-11-21 23:52:09 +0700

    cập nhật hoàn chỉnh header và footer

---
+++ script.js
@@ -0,0 +1,7 @@
+// Make a toggle search bar when click the search icons
+$(document).ready(function () {
+  $(".search-button").click(function () {
+    $(".search-input").toggle("slide");
+    $(".nav.login-menu").toggleClass("shift-left");
+  });
+});
